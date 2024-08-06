5e363be483607e8f7eb1cf99df20710ca09267b7
Aetopia <41850963+Aetopia@users.noreply.github.com>
2024-08-06 13:21:58 +0530

    Improved checks.

--- Class.cs
+++ Class.cs
@@ -3,6 +3,7 @@ namespace StoreEdge;
 using System;
 using System.IO;
 using System.Xml;
+using System.Net;
 using System.Text;
 using System.Linq;
 using Windows.System;
@@ -11,12 +12,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using Windows.System.UserProfile;
 using System.Collections.Generic;
+using Windows.Management.Deployment;
 using System.Collections.ObjectModel;
 using System.Runtime.InteropServices;
-using System.Runtime.Serialization.Json;
-using Windows.Management.Deployment;
 using System.Web.Script.Serialization;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Json;
 
 readonly struct Resources
 {
@@ -32,18 +33,18 @@ readonly struct Resources
     }
 }
 
-readonly struct SynchronizationContextRemover : INotifyCompletion
+readonly struct _ : INotifyCompletion
 {
     internal readonly bool IsCompleted => SynchronizationContext.Current == null;
 
     internal readonly void GetResult() { }
 
-    internal readonly SynchronizationContextRemover GetAwaiter() { return this; }
+    internal readonly _ GetAwaiter() { return this; }
 
-    public readonly void OnCompleted(Action continuation)
+    public readonly void OnCompleted(Action _)
     {
         var syncContext = SynchronizationContext.Current;
-        try { SynchronizationContext.SetSynchronizationContext(null); continuation(); }
+        try { SynchronizationContext.SetSynchronizationContext(null); _(); }
         finally { SynchronizationContext.SetSynchronizationContext(syncContext); }
     }
 }
@@ -107,27 +108,35 @@ public static class Store
 
     static readonly string requestUri = $"https://storeedgefd.dsx.mp.microsoft.com/v9.0/products{{0}}?market={GlobalizationPreferences.HomeGeographicRegion}&locale=iv&deviceFamily=Windows.Desktop";
 
-    static readonly ((string Native, string Compatible) OS, (ProcessorArchitecture Native, ProcessorArchitecture Compatible) Processor) architectures = (
-        (RuntimeInformation.OSArchitecture.ToString().ToLowerInvariant(), RuntimeInformation.OSArchitecture switch { Architecture.X64 => "x86", Architecture.Arm64 => "arm", _ => null }),
-        (RuntimeInformation.OSArchitecture switch
-        {
-            Architecture.X86 => ProcessorArchitecture.X86,
-            Architecture.X64 => ProcessorArchitecture.X64,
-            Architecture.Arm => ProcessorArchitecture.Arm,
-            Architecture.Arm64 => ProcessorArchitecture.Arm64,
-            _ => ProcessorArchitecture.Unknown
-        },
-        RuntimeInformation.OSArchitecture switch
-        {
-            Architecture.X64 => ProcessorArchitecture.X86,
-            Architecture.Arm64 => ProcessorArchitecture.Arm,
-            _ => ProcessorArchitecture.Unknown
-        })
+    static readonly (
+        (string String, ProcessorArchitecture Architecture) Native,
+        (string String, ProcessorArchitecture Architecture) Compatible
+    ) architectures = (
+        (
+            RuntimeInformation.OSArchitecture.ToString().ToLowerInvariant(),
+            RuntimeInformation.OSArchitecture switch
+            {
+                Architecture.X86 => ProcessorArchitecture.X86,
+                Architecture.X64 => ProcessorArchitecture.X64,
+                Architecture.Arm => ProcessorArchitecture.Arm,
+                Architecture.Arm64 => ProcessorArchitecture.Arm64,
+                _ => ProcessorArchitecture.Unknown
+            }
+        ),
+        (
+            RuntimeInformation.OSArchitecture switch { Architecture.X64 => "x86", Architecture.Arm64 => "arm", _ => null },
+            RuntimeInformation.OSArchitecture switch
+            {
+                Architecture.X64 => ProcessorArchitecture.X86,
+                Architecture.Arm64 => ProcessorArchitecture.Arm,
+                _ => ProcessorArchitecture.Unknown
+            }
+        )
     );
 
     public static async Task<string> GetUrlAsync(UpdateIdentity update)
     {
-        await default(SynchronizationContextRemover);
+        await default(_);
 
         return (await PostAsSoapAsync(string.Format(Resources.GetExtendedUpdateInfo2, update.UpdateID, update.RevisionNumber), true))
        .GetElementsByTagName("Url")
@@ -137,7 +146,7 @@ public static class Store
 
     public static async Task<ReadOnlyCollection<string>> GetPackageFamilyNamesAsync(params string[] packageFamilyNames)
     {
-        await default(SynchronizationContextRemover);
+        await default(_);
 
         using StringContent content = new(serializer.Serialize(new Dictionary<string, object>()
         {
@@ -148,31 +157,29 @@ public static class Store
         using var message = await client.PostAsync(string.Format(requestUri, string.Empty), content);
         message.EnsureSuccessStatusCode();
 
-        return new(Deserialize(await message.Content.ReadAsStringAsync()).GetElementsByTagName("ProductId").Cast<XmlNode>().Select(element => element.InnerText).ToArray());
+        return new(Deserialize(await message.Content.ReadAsByteArrayAsync()).GetElementsByTagName("ProductId").Cast<XmlNode>().Select(element => element.InnerText).ToArray());
     }
 
     public static async Task<ReadOnlyCollection<Product>> GetProductsAsync(params string[] productIds)
     {
-        await default(SynchronizationContextRemover);
+        await default(_);
 
         Product[] products = new Product[productIds.Length];
 
         for (int i = 0; i < products.Length; i++)
         {
-            var productId = productIds[i];
-
-            using var message = await client.GetAsync(string.Format(requestUri, $"/{productId}"));
+            using var message = await client.GetAsync(string.Format(requestUri, $"/{productIds[i]}"));
             message.EnsureSuccessStatusCode();
 
-            var payload = Deserialize(await message.Content.ReadAsStringAsync())["Payload"];
+            var payload = Deserialize(await message.Content.ReadAsByteArrayAsync())["Payload"];
             var title = payload?["ShortTitle"]?.InnerText;
-            var enumerable = payload["Platforms"].Cast<XmlNode>().Select(node => node.InnerText);
+            var platforms = payload["Platforms"].Cast<XmlNode>().Select(node => node.InnerText);
 
             products[i] = new(
                 string.IsNullOrEmpty(title) ? payload["Title"].InnerText : title,
-                (enumerable.FirstOrDefault(item => item.Equals(architectures.OS.Native, StringComparison.OrdinalIgnoreCase)) ??
-                enumerable.FirstOrDefault(item => item.Equals(architectures.OS.Compatible, StringComparison.OrdinalIgnoreCase)))?.ToLowerInvariant(),
-                Deserialize(payload.GetElementsByTagName("FulfillmentData")[0].InnerText)["WuCategoryId"].InnerText
+                (platforms.FirstOrDefault(item => item.Equals(architectures.Native.String, StringComparison.OrdinalIgnoreCase)) ??
+                platforms.FirstOrDefault(item => item.Equals(architectures.Compatible.String, StringComparison.OrdinalIgnoreCase)))?.ToLowerInvariant(),
+                Deserialize(Encoding.Unicode.GetBytes(payload.GetElementsByTagName("FulfillmentData")[0].InnerText))["WuCategoryId"].InnerText
             );
         }
 
@@ -181,19 +188,23 @@ public static class Store
 
     public static async Task<ReadOnlyCollection<UpdateIdentity>> GetUpdatesAsync(Product product)
     {
-        await default(SynchronizationContextRemover);
+        await default(_);
 
         if (product.Architecture is null) return new([]);
+
         var result = (XmlElement)(await PostAsSoapAsync(
             string.Format(
                 content ??= string.Format(Resources.GetString("SyncUpdates.xml"),
                 (await PostAsSoapAsync(Resources.GetString("GetCookie.xml"))).GetElementsByTagName("EncryptedData")[0].InnerText, "{0}"),
-                product.AppCategoryId)))
+                product.AppCategoryId), false))
             .GetElementsByTagName("SyncUpdatesResult")[0];
+
+        var nodes = result.GetElementsByTagName("AppxPackageInstallData");
+        if (nodes.Count == 0) return new([]);
 
         ProcessorArchitecture architecture;
         Dictionary<string, Update> dictionary = [];
-        foreach (XmlNode node in result.GetElementsByTagName("AppxPackageInstallData"))
+        foreach (XmlNode node in nodes)
         {
             var element = (XmlElement)node.ParentNode.ParentNode.ParentNode;
             var file = element.GetElementsByTagName("File")[0];
@@ -201,7 +212,7 @@ public static class Store
 
             var identity = file.Attributes["InstallerSpecificIdentifier"].InnerText.Split('_');
             var neutral = identity[2] == "neutral";
-            if (!neutral && identity[2] != architectures.OS.Native && identity[2] != architectures.OS.Compatible) continue;
+            if (!neutral && identity[2] != architectures.Native.String && identity[2] != architectures.Compatible.String) continue;
             architecture = (neutral ? product.Architecture : identity[2]) switch
             {
                 "x86" => ProcessorArchitecture.X86,
@@ -229,9 +240,13 @@ public static class Store
         }
 
         var values = dictionary.Where(item => item.Value.MainPackage).Select(item => item.Value);
-        architecture = (values.FirstOrDefault(value => value.Architecture == architectures.Processor.Native) ?? values.FirstOrDefault(value => value.Architecture == architectures.Processor.Compatible)).Architecture;
+        architecture = (
+            values.FirstOrDefault(value => value.Architecture == architectures.Native.Architecture) ??
+            values.FirstOrDefault(value => value.Architecture == architectures.Compatible.Architecture)
+        ).Architecture;
         var items = dictionary.Select(item => item.Value).Where(item => item.Architecture == architecture);
-        List<UpdateIdentity> updates = [];
+
+        List<UpdateIdentity> list = [];
 
         foreach (XmlNode node in result.GetElementsByTagName("SecuredFragment"))
         {
@@ -245,31 +260,32 @@ public static class Store
                 new Version(item.Version) > new Version(package.Id.Version.Major, package.Id.Version.Minor, package.Id.Version.Build, package.Id.Version.Revision)))
             {
                 var attributes = element.GetElementsByTagName("UpdateIdentity")[0].Attributes;
-                updates.Add(new(attributes["UpdateID"].InnerText, attributes["RevisionNumber"].InnerText, item.MainPackage));
+                list.Add(new(attributes["UpdateID"].InnerText, attributes["RevisionNumber"].InnerText, item.MainPackage));
             }
             else if (item.MainPackage) return new([]);
         }
 
-        updates.Sort((x, y) => x.MainPackage ? 1 : -1);
-        return updates.AsReadOnly();
+        list.Sort((x, y) => x.MainPackage ? 1 : -1);
+        return new(list);
     }
 
-    static XmlElement Deserialize(string input)
+    static XmlElement Deserialize(byte[] buffer)
     {
-        using var reader = JsonReaderWriterFactory.CreateJsonReader(Encoding.UTF8.GetBytes(input), XmlDictionaryReaderQuotas.Max);
+        using var reader = JsonReaderWriterFactory.CreateJsonReader(buffer, XmlDictionaryReaderQuotas.Max);
         XmlDocument document = new();
         document.Load(reader);
         return document["root"];
     }
 
-    static async Task<XmlDocument> PostAsSoapAsync(string _, bool secured = false)
+    static async Task<XmlDocument> PostAsSoapAsync(string value, bool? _ = null)
     {
-        using StringContent content = new(_, Encoding.UTF8, "application/soap+xml");
-        using var message = await client.PostAsync(secured ? "secured" : null, content);
+        using StringContent content = new(value, Encoding.UTF8, "application/soap+xml");
+        using var message = await client.PostAsync(_.HasValue && _.Value ? "secured" : null, content);
         message.EnsureSuccessStatusCode();
+        var xml = await message.Content.ReadAsStringAsync();
 
         XmlDocument document = new();
-        document.LoadXml((await message.Content.ReadAsStringAsync()).Replace("&lt;", "<").Replace("&gt;", ">"));
+        document.LoadXml(_.HasValue && !_.Value ? WebUtility.HtmlDecode(xml) : xml);
         return document;
     }
 }
